b03bfbb389d511a9c23f3d22aae4e19dd37c6a41
4wiskow <marian.wiskow@protonmail.com>
2021-01-31 19:45:29 +0100

    Added comments, shapiro test for normality of mean distributions of valence, arousal

--- subjects_analysis.py
+++ subjects_analysis.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
 CSV_FILE_NAME = "data_Song_Lyrics_Gr6_2021-01-31_18-33.csv"
 
@@ -11,30 +12,49 @@ with open(CSV_FILE_NAME, "r", encoding="utf16") as csv_file:
     rows = [r for r in reader]
     df = pd.DataFrame(rows[1:], columns=header)
 
-# BFI responses normally distributed?
-bfi_df = df.filter(regex="BF02").astype("int32")
-bfi_df.hist(xlabelsize=0, ylabelsize=10, sharey=True,)
-plt.show()
-
-# Dep Variable normally distributed across participants?
-val_df = df.filter(regex="VA").astype("int32")
-val_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
-plt.show()
-
-# Dep Variable normally distributed across participants?
-ar_df = df.filter(regex="^(AR)").astype("int32")
-ar_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
-plt.show()
-
 ## Time
-# sosci computed score for fast completion > 100 indicates low-quality data.
+# sosci-computed scores for fast completion
 deg_time = df["DEG_TIME"].astype("int32")
 deg_time[deg_time > 100].empty  # no values > 100, nothing to exclude
 
 time_rsi = df["TIME_RSI"].astype("double")
 time_rsi[time_rsi > 2.].empty  # no values > 2., nothing to exclude
 
-# time taken for study should be more than 20 min
+# it should take at least 20 min to complete the study
 time_sum = df["TIME_SUM"].astype("int32")
-time_sum[time_sum < 20*60].index  # subject 32 should be excluded
+time_sum[time_sum < 20*60].index  # participant 32 should be excluded
+time_sum.max() / 60  # longest time time to completion 60 min
+
+## Response Data
+# BFI responses normally distributed?
+bfi_df = df.filter(regex="BF02").astype("int32")
+bfi_df.hist(xlabelsize=0, ylabelsize=10, sharey=True,)
+plt.title("BFI 10 Response Distributions")
+plt.show()
+
+# Dep Variable normally distributed across participants?
+# Participants indicated for exclusion: 17, 39 and 40
+ar_df = df.filter(regex="^(AR)").astype("int32")
+ar_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
+plt.suptitle("Arousal Response Distribution per Participant")
+plt.show()
+
+# Dep Variable normally distributed across participants?
+# Participants indicated for exclusion: 17, 39 and 40
+val_df = df.filter(regex="VA").astype("int32")
+val_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
+plt.suptitle("Valence Response Distribution per Participant")
+plt.show()
+
+# Arousal Mean across participants
+ar_df.mean().hist()
+plt.title("Mean Arousal Response")
+plt.show()
+stats.shapiro(ar_df.mean())
+
+# Valence Mean across participants
+val_df.mean().hist()
+plt.title("Mean Valence Response")
+plt.show()
+stats.shapiro(val_df.mean())  # p < .5, not normally distributed!
 
