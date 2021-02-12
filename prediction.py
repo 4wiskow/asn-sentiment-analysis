a5e32e4a24d3d1aa162996c36b0c4b3eaa887075
4wiskow <marian.wiskow@protonmail.com>
2021-02-12 22:06:59 +0100

    added accuracy, f1 scores for classification

--- prediction.py
+++ prediction.py
@@ -1,10 +1,9 @@
 from scipy.stats import stats
-from seaborn import jointplot
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import r2_score, f1_score, accuracy_score
 import pandas as pd
 import seaborn as sns
-from sklearn import linear_model
+
 from SentiArtBased import calc_aap
 from vader import calc_vader_scores
 from subjects_analysis import read_sosci, CSV_FILE_NAME, DROP_PARTICIPANTS
@@ -17,30 +16,41 @@ val_df = df.filter(regex="VA").astype("int32")
 ar_means = ar_df.mean()
 val_means = val_df.mean()
 
-sa_lines, sentiArt_hit_rate = calc_aap()
+sa_lines = calc_aap()
+sa_hit_rate = sa_lines["hit_rate"].mean()
+sa_lines["aap_label"] = (sa_lines["aap"] >= 0).astype("int32")
 sa_lines["aap_post_z"] = stats.zscore(sa_lines["aap"])
 sa_lines["val_z_ratings"] = stats.zscore(val_means.values)
 sa_lines["ar_z_ratings"] = stats.zscore(ar_means.values)
 
-# Metrics
-r = sa_lines.corr().iloc[0, 1]  # do other metrics even make sense?
-mse = mean_squared_error(sa_lines["aap_post_z"], sa_lines["val_z_ratings"])
-r2 = r2_score(sa_lines["aap_post_z"], sa_lines["val_z_ratings"])
-sns.lmplot(x="val_z_ratings", y="ar_z_ratings", data=sa_lines, order=2)
-plt.tight_layout()
-plt.show()
 
 ## Vader
-vader_lines, vader_hit_rate = calc_vader_scores()
+vader_lines = calc_vader_scores()
+vader_hit_rate = vader_lines["hit_rate"].mean()
 predictions_lines = pd.concat([sa_lines, vader_lines.drop("text", axis=1)], axis=1)
+#print("predictions_lines vader", predictions_lines)
+#print("VADER", vader_hit_rate)
 
-# Time Series
-aap_smooth = sa_lines.aap_post_z.rolling(window=5).mean()
+# Affective Time Series
+aap_smooth = sa_lines.aap.rolling(window=5).mean()
 val_smooth = sa_lines.val_z_ratings.rolling(window=5).mean()
 plt.plot(range(len(sa_lines)), aap_smooth, val_smooth)
 plt.title("AAP and Valence Time Series (Smoothed)")
 plt.xlabel("Line")
 plt.ylabel("y")
+plt.legend(["aap", "val"])
 plt.show()
-print("predictions_lines vader", predictions_lines)
-print("VADER",vader_hit_rate)
+
+## Metrics
+# regression
+r = predictions_lines[["val_z_ratings", "aap", "vader_compound"]].corr()
+#predictions_lines[["val_z_ratings", "aap", "vader_compound"]].plot()
+r2 = r2_score(sa_lines["aap_post_z"], sa_lines["val_z_ratings"])
+
+# classification
+predictions_lines["val_sign"] = (predictions_lines["val_z_ratings"] >= 0).astype("int32")
+vader_acc = accuracy_score(predictions_lines["val_sign"], predictions_lines["vader_label"])
+sa_acc = accuracy_score(predictions_lines["val_sign"], predictions_lines["aap_label"])
+
+sa_f1 = f1_score(predictions_lines["val_sign"], predictions_lines["aap_label"])
+vader_f1 = f1_score(predictions_lines["val_sign"], predictions_lines["vader_label"])
--- vader.py
+++ vader.py
@@ -6,7 +6,7 @@ Vader basic sentiment analysis tool for literary texts
 """
 # get packages/requirements
 import SentiArtBased
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+#from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score, accuracy_score
@@ -32,9 +32,10 @@ def scores(df):
     """Score text based on VADER"""
     # Create Prediction column based on Polarity Score
     # converts negative compound scores to -1 and positive compound scores to 1
-    df['Prediction'] = df['text'].apply(lambda x: 1 if vader.polarity_scores(x)['compound'] >= 0 else -1)
-    df['pos'] = df['text'].apply(lambda x: vader.polarity_scores(x)['pos'])
-    df['neg'] = df['text'].apply(lambda x: vader.polarity_scores(x)['neg'])
+    df['vader_compound'] = df['text'].apply(lambda x: vader.polarity_scores(x)['compound'])
+    df['vader_label'] = (df["vader_compound"] >= 0).astype("int32")
+    df['vader_pos'] = df['text'].apply(lambda x: vader.polarity_scores(x)['pos'])
+    df['vader_neg'] = df['text'].apply(lambda x: vader.polarity_scores(x)['neg'])
     #df['neutral'] = df['text'].apply(lambda x: vader.polarity_scores(x)['neu'])
     #df['score'] = df['text'].apply(lambda x: analyzer.polarity_scores(x)['compound'])
     print(df.head())
@@ -59,25 +60,17 @@ def get_vader_lexicon():
 
 def calc_vader_scores():
     """Calculate scores and hit rate based on VADER"""
-    file = ""
     vader_scores = pd.DataFrame()
-    from subjects_analysis import df_val_mean
     for file in SentiArtBased.SONGS_ORDERED:
         print("File", file)
         df = read_data(file)
-        if file == 'dylan_low_lit/going_going_gone.txt':
-            df['Truth'] = df_val_mean.iloc[0:32][0].round(2)
-        if file == 'dylan_low_lit/lay_lady_lay.txt':
-            df['Truth'] = df_val_mean.iloc[32:51][0].round(2)
-        if file == 'dylan_low_lit/i_threw_it_all_away.txt':
-            df['Truth'] = df_val_mean.iloc[51:71][0].round(2)
-        if file == 'dylan_low_lit/abandoned_love.txt':
-            df['Truth'] = df_val_mean.iloc[71:104][0].round(2)
         prediction_values = scores(df)
         vader_scores = vader_scores.append(prediction_values)
-        plot(prediction_values, file[:-4])
+        #plot(prediction_values, file[:-4])
 
     vader_scores = vader_scores.reset_index(drop=True)
-    hit_rate = SentiArtBased.get_hit_rate(get_vader_lexicon().keys())
-    print("Hit rate Vader:", hit_rate)
-    return vader_scores, hit_rate
+    tokens = SentiArtBased.get_tokens_per_line(SentiArtBased.SONGS_ORDERED)
+    hit_rate = SentiArtBased.per_line_hit_rate(tokens, get_vader_lexicon().keys())
+    vader_scores["hit_rate"] = hit_rate
+    #print("Hit rate Vader:", hit_rate)
+    return vader_scores
