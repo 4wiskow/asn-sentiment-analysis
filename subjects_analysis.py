08ec238ad8c7188be1c71f1abdbfd843d953ef97
4wiskow <marian.wiskow@protonmail.com>
2021-02-12 22:07:09 +0100

    uncommented plotting

--- subjects_analysis.py
+++ subjects_analysis.py
@@ -1,11 +1,8 @@
 import csv
 import pandas as pd
-import matplotlib.pyplot as plt
-from SentiArtBased import calc_aap
-from vader import calc_vader_scores
 from scipy import stats
-from sklearn.metrics import mean_squared_error, r2_score
-from seaborn import jointplot
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 CSV_FILE_NAME = "data/data_Song_Lyrics_Gr6_2021-01-31_18-33.csv"
 
@@ -41,15 +38,20 @@ time_sum.max() / 60  # longest time time to completion 60 min
 ## Response Data
 # BFI responses normally distributed?
 bfi_df = df.filter(regex="(BF02_)0?([1-9]|10)$").astype("int32")  # leave out mysterious 11th BFI question
-bfi_df.hist(xlabelsize=0, ylabelsize=10, sharey=True,)
-plt.title("BFI 10 Response Distributions")
+#bfi_df.hist(xlabelsize=0, ylabelsize=10, sharey=True,)
+#plt.title("BFI 10 Response Distributions")
 #plt.tight_layout()
 #plt.show()
 
+ar_df = df.filter(regex="^(AR)").astype("int32")
+ar_means = ar_df.drop(DROP_PARTICIPANTS, axis=0).mean()
+val_df = df.filter(regex="VA").astype("int32")
+val_means = val_df.drop(DROP_PARTICIPANTS, axis=0).mean()
+ratings = pd.DataFrame(list(zip(val_means, ar_means)), columns=["val", "ar"])
+
 # Dep Variable normally distributed across participants?
 # Participants indicated for exclusion: 17, 39 and 40
-ar_df = df.filter(regex="^(AR)").astype("int32")
-ar_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
+#ar_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
 #plt.suptitle("Arousal Response Distribution per Participant")
 #plt.show()
 
@@ -58,8 +60,7 @@ ar_by_participant[abs(stats.zscore(ar_by_participant)) > 2]  # no. 6 is beyond 2
 
 # Dep Variable normally distributed across participants?
 # Participants indicated for exclusion: 17, 39 and 40
-val_df = df.filter(regex="VA").astype("int32")
-val_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
+#val_df.transpose().hist(xlabelsize=0, ylabelsize=10, sharey=True, figsize=(15, 10))
 #plt.suptitle("Valence Response Distribution per Participant")
 #plt.show()
 
@@ -68,28 +69,26 @@ val_by_participant = val_df.transpose().mean()
 val_by_participant[abs(stats.zscore(val_by_participant)) > 2]  # participants 6, 42, 46 are beyond 2 sd
 
 # Arousal Mean across participants
-ar_means = ar_df.drop(DROP_PARTICIPANTS, axis=0).mean()
-df_ar_mean = pd.DataFrame([ar_means]).transpose()
-df_ar_mean = df_ar_mean.reset_index()
-#print("Song 2",  df_ar_mean.iloc[32:51])
-#print("Song 3",  df_ar_mean.iloc[51:71])
-#print("Song 4",  df_ar_mean.iloc[71:104])
-ar_means.plot.density()
-plt.title("Mean Arousal Response")
-plt.show()
+#ar_means.plot.density()
+#plt.title("Mean Arousal Response")
+#plt.show()
 stats.shapiro(ar_df.mean())
 
 
 # Valence Mean across participants
-val_means = val_df.drop(DROP_PARTICIPANTS, axis=0).mean()
-df_val_mean = pd.DataFrame([val_means]).transpose()
-df_val_mean = df_val_mean.reset_index()
-val_means.plot.density()
-plt.title("Mean Valence Response")
-plt.show()
+#val_means.plot.density()
+#plt.title("Mean Valence Response")
+#plt.show()
 stats.shapiro(val_means)  # p < .5, not normally distributed!
 
-h = jointplot(x=val_means.values, y=ar_means.values)
-h.set_axis_labels("valence ratings", "arousal ratings")
-plt.tight_layout()  # no inverse U?
+#h = jointplot(x=val_means.values, y=ar_means.values)
+#h.set_axis_labels("valence ratings", "arousal ratings")
+#plt.tight_layout()  # no inverse U?
+#plt.show()
+
+ratings = pd.DataFrame(list(zip(val_means, ar_means)), columns=["val", "ar"])
+ratings["val_z"] = stats.zscore(ratings["val"])
+ratings["ar_z"] = stats.zscore(ratings["ar"])
+sns.lmplot(x="val_z", y="ar_z", data=ratings, order=2)
+plt.tight_layout()
 plt.show()
