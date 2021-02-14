142d51cfb6b818649df628335f25d4b8694da23f
4wiskow <marian.wiskow@protonmail.com>
2021-02-14 12:35:35 +0100

    added BFI and Language regressions

--- prediction.py
+++ prediction.py
@@ -2,20 +2,38 @@ from scipy.stats import stats
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score, f1_score, accuracy_score
 import pandas as pd
-import seaborn as sns
+import statsmodels.api as sm
 
 from SentiArtBased import calc_aap
+import data
 from vader import calc_vader_scores
-from subjects_analysis import read_sosci, CSV_FILE_NAME, DROP_PARTICIPANTS
 
-df = read_sosci(CSV_FILE_NAME)
-df = df.drop(DROP_PARTICIPANTS, axis=0)
+df = data.read_sosci(data.CSV_FILE_NAME)
+df = df.drop(data.DROP_PARTICIPANTS, axis=0)
 ar_df = df.filter(regex="^(AR)").astype("int32")
 val_df = df.filter(regex="VA").astype("int32")
 
 ar_means = ar_df.mean()
 val_means = val_df.mean()
 
+
+## Regressions
+# BFI -> Valence Regression
+ocean = data.read_ocean()
+ols_bfi = sm.OLS(endog=val_df.transpose().mean().values,
+                 exog=sm.add_constant(ocean))  # have to add intercept term manually
+res_bfi = ols_bfi.fit()
+res_bfi.summary()  # all dimensions insignificant
+
+# Language -> Valence Regression
+lang = data.read_language()
+ols_lang = sm.OLS(endog=val_df.transpose().mean().values,
+                  exog=sm.add_constant(lang["language"]), missing="drop")
+res_lang = ols_lang.fit()
+res_lang.summary()  # insignificant
+
+
+## SATs
 sa_lines = calc_aap()
 sa_hit_rate = sa_lines["hit_rate"].mean()
 sa_lines["aap_label"] = (sa_lines["aap"] >= 0).astype("int32")
@@ -23,13 +41,12 @@ sa_lines["aap_post_z"] = stats.zscore(sa_lines["aap"])
 sa_lines["val_z_ratings"] = stats.zscore(val_means.values)
 sa_lines["ar_z_ratings"] = stats.zscore(ar_means.values)
 
-
 ## Vader
 vader_lines = calc_vader_scores()
 vader_hit_rate = vader_lines["hit_rate"].mean()
 predictions_lines = pd.concat([sa_lines, vader_lines.drop("text", axis=1)], axis=1)
-#print("predictions_lines vader", predictions_lines)
-#print("VADER", vader_hit_rate)
+# print("predictions_lines vader", predictions_lines)
+# print("VADER", vader_hit_rate)
 
 # Affective Time Series
 aap_smooth = sa_lines.aap.rolling(window=5).mean()
@@ -41,16 +58,17 @@ plt.ylabel("y")
 plt.legend(["aap", "val"])
 plt.show()
 
-## Metrics
-# regression
+# Metrics
+# valence regression
 r = predictions_lines[["val_z_ratings", "aap", "vader_compound"]].corr()
-#predictions_lines[["val_z_ratings", "aap", "vader_compound"]].plot()
+# predictions_lines[["val_z_ratings", "aap", "vader_compound"]].plot()
 r2 = r2_score(sa_lines["aap_post_z"], sa_lines["val_z_ratings"])
 
-# classification
+# valence sign classification
 predictions_lines["val_sign"] = (predictions_lines["val_z_ratings"] >= 0).astype("int32")
 vader_acc = accuracy_score(predictions_lines["val_sign"], predictions_lines["vader_label"])
 sa_acc = accuracy_score(predictions_lines["val_sign"], predictions_lines["aap_label"])
 
 sa_f1 = f1_score(predictions_lines["val_sign"], predictions_lines["aap_label"])
 vader_f1 = f1_score(predictions_lines["val_sign"], predictions_lines["vader_label"])
+
