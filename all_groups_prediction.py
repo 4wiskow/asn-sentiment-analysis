91cfc35fb4bb8c3a66f950707e95557d2e9a3480
4wiskow <marian.wiskow@protonmail.com>
2021-02-22 19:56:23 +0100

    Added KFold crossval, changed valence regression to group zscores

--- all_groups_prediction.py
+++ all_groups_prediction.py
@@ -4,34 +4,51 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 import numpy as np
 
 val_open = data.all_by_participant()
-plt.plot(val_open.index, val_open["val_z"])
+plt.plot(val_open.index, val_open["val_z_groups"])
 plt.show()
 
 val_open.corr()  # some correlation for wellknown_dylan x valence
-sns.lmplot(data=val_open, y="val_z", x="wellknown_dylan", x_jitter=.1)
+sns.lmplot(data=val_open, y="val_z_groups", x="wellknown_dylan", x_jitter=.1)
+plt.xticks([1., 2., 3., 4.], ["Not at all", "Somewhat", "Well", "Very Well"])
+plt.title("Familiarity With Dylan vs. Valence")
+plt.tight_layout()
+plt.show()
+
+sns.catplot(x="native_language_str", y="val_z_groups", kind="box", data=val_open.replace("English-German Bilingual", "Bilingual"))
+plt.title("Native Language vs. Valence Across All Groups")
+plt.xlabel("Native Language")
+plt.tight_layout()
 plt.show()
 
 # Linear Regression: Openness, Group -> Valence
-ols = sm.ols("val ~ wellknown_dylan + C(native_language_str) + C(group)", data=val_open)
+val_open["val_z_abs"] = val_open["val_z_groups"].abs()
+ols = sm.ols("val_z_groups ~ C(wellknown_dylan)  + C(native_language_str) + C(group)", data=val_open)
 res = ols.fit()
 res.summary()  # wellknown dylan p < .5, native language p < .01
 
 
 # NN
-mlp = MLPRegressor((10,), activation='relu', random_state=1, max_iter=5000)
 enc = OneHotEncoder()
 d = val_open.loc[:, ["wellknown_dylan", "native_language_str", "group"]]
 enc.fit(d)
 x = enc.transform(d).toarray()
-y = val_open.loc[:, "val"].to_numpy()
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+y = val_open.loc[:, "val_z_groups"].to_numpy()
 
-mlp.fit(x_train, y_train)
-mlp.score(x_test, y_test)
+kfold = KFold(5)
+scores = []
+for train_idx, test_idx in kfold.split(x, y):
+    x_train, x_test = x[train_idx], x[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+
+    mlp = MLPRegressor((100, 50), activation='relu', max_iter=5000)
+    mlp.fit(x_train, y_train)
+    s = mlp.score(x_test, y_test)
+    scores.append(s)
+np.mean(scores)
 
 
 ## By Line
